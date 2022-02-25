16329348f2435d4a2d951ad6aea7c0fcae98fedc
Cid <59629922+cid-5050@users.noreply.github.com>
2022-02-25 17:19:23 +0100

    invertir

--- invertir.h
+++ invertir.h
@@ -1,19 +1,22 @@
-#ifndef INVERTIR_H
-#define INVERTIR_H
+#include <string>
 
-string invertir(string const& texto){
-    string resultado;
-    
-    //SIN ITERADORES
+#ifndef INVERTIR_H_
+#define INVERTIR_H_
+
+
+std::string invertir(std::string const& texto) {
+    std::string resultado;
+
+    // SIN ITERADORES
     // for(int i = texto.size()-1; i>=0; i--){
     //    resultado += texto.at(i);
     //}
-    
-    //CON ITERADORES
-    for(auto letra = texto.end()-1; letra>=texto.begin(); --letra){
+
+    // CON ITERADORES
+    for(auto letra = texto.end()-1; letra >= texto.begin(); --letra) {
         resultado += *letra;
     }
     return resultado;
 }
 
-#endif // INVERTIR_H
+#endif  // INVERTIR_H_
