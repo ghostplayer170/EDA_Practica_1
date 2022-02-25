d4cc34c96211ab1d3527526db9d9ce3cf7f4313a
Cid <59629922+cid-5050@users.noreply.github.com>
2022-02-25 17:22:48 +0100

    vectores

--- vectoresIguales.h
+++ vectoresIguales.h
@@ -1,18 +1,21 @@
+#include <vector>
+
 #ifndef VECTORESIGUALES_H_
 #define VECTORESIGUALES_H_
 
-bool vec_iguales (vector<int> const& vecA, vector<int> const& vecB){
-    bool igualdad=true;
-    if(vecA.size()==vecB.size()) {
-        for(int i{0}; i<vecA.size(); i++){
-            if(vecA.at(i)!=vecB.at(i)){
-                igualdad=false;
+
+bool vec_iguales (std::vector<int> const& vecA, std::vector<int> const& vecB) {
+    bool igualdad = true;
+    if(vecA.size() == vecB.size()) {
+        for(int i{0}; i < vecA.size(); i++) {
+            if(vecA.at(i) != vecB.at(i)) {
+                igualdad = false;
                 return igualdad;
             }
         }
         return igualdad;
     }
-    return igualdad=false;
+    return igualdad = false;
 }
 
 #endif  // VECTORESIGUALES_H_
