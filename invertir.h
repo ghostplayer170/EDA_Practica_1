59712c2ca7770a80146e4c45cd10c3af60fb8826
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2022-02-25 17:15:48 +0100

    Update invertir.h

--- invertir.h
+++ invertir.h
@@ -1,6 +1,19 @@
-#ifndef INVERTIR_H_
-#define INVERTIR_H_
+#ifndef INVERTIR_H
+#define INVERTIR_H
 
+string invertir(string const& texto){
+    string resultado;
+    
+    //SIN ITERADORES
+    // for(int i = texto.size()-1; i>=0; i--){
+    //    resultado += texto.at(i);
+    //}
+    
+    //CON ITERADORES
+    for(auto letra = texto.end()-1; letra>=texto.begin(); --letra){
+        resultado += *letra;
+    }
+    return resultado;
+}
 
-
-#endif  // INVERTIR_H_
+#endif // INVERTIR_H
